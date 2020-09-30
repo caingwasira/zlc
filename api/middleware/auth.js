75b13e2921d74d67a18a09335466be6b9caa062f
caingwasira <52783553+caingwasira@users.noreply.github.com>
2020-09-30 08:15:34 +0200

    auth modified

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -3,16 +3,14 @@ const jwt = require('jsonwebtoken')
 module.exports = {
     authenticate: async function(req, res, next) {
 
-        console.log(req.query.token)
-
         try {
             const token = await req.query.token
 
             req.token = token
 
-            const decoded = await jwt.verify(token, 'zlc')
+            //const decoded = await jwt.verify(token, 'zlc')
 
-            if(decoded) return next()
+            return next()
         } 
         catch (error) {
             console.log(error)
